fdb0d9dcfdf3743b87e0d21398d034df07603009
Syuan <carsun00@gmail.com>
2020-08-24 17:09:05 +0800

    CH2 - 單元測試的藝術 HackMD - https://hackmd.io/@Syuan/單元測試的藝術 差異：  1.  書中所使用的Nunit 2.0版本，在新的Nunit 3.0中部分語法已經被移除，修正無法使用的語法。  2. 修正書中勘誤的地方

---
+++ MemCalculator.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace MemCalculator.UnitTests
+{
+    [TestFixture()]
+    public class MemCalculatorTests
+    {
+        #region 1.1 預設值測試
+
+        [Test()]
+        public void Sum_ByDefault_ReturnZero()
+        {
+            MemCalculator calc = new MemCalculator();
+            int lastSum = calc.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+        #endregion
+
+        #region 1.2 數值測試
+        [Test()]
+        public void Add_WhenCalled_ChangeSum()
+        {
+            MemCalculator calc = MakeCalc();
+            calc.Add(1);
+            int sum = calc.Sum();
+            Assert.AreEqual(1, sum);
+        }
+        /// <summary>
+        ///     使用工廠方法初始化物件，
+        ///     優點
+        ///     1.  提升程式可讀性
+        ///     2.  保證每個物件都是以相同的方式建立。
+        ///     3.  如果有多個地方使用上，只需要在一個地方修改程式碼，
+        ///         提升測試的可維護性。
+        /// </summary>
+        /// <returns></returns>
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+        #endregion
+    }
+}
